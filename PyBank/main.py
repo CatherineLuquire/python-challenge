e7273419d172262fbbd46295ca7afe0d872a87a4
Catherine Luquire <catherineluquire@Catherines-MacBook-Air-2.local>
2020-07-07 17:13:01 -0400

    completed project

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,76 +1,55 @@
 #create python script that analyzes the records 
+#import os and csv
 import os
 
 csvpath = os.path.join('resources', 'budget_data.csv')
 import csv
 
-print("Financial Analysis")
-print("----------------------------")
-
-# total_months = 0
-# total_pl = 0
+# create lists to store revenue, dates, and monthly revenue change 
 revenue = []
 date = []
 rev_change_lst = []
-# greatest_inc = 0
-# greatest_dec = 0
 
-#with open(csvpath) as csvfile:
+# with open(csvpath) as csvfile:
 with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
-    # print(csvreader)
+    # pass csv header
     csv_header = next(csvreader) 
-    # print(csv_header)
 
-    # calculate the net total amount of "Profit/Losses" over the entire period
+    # create loop to populate the date and revenue lists with dates and revenues from csv file
     for row in csvreader:
         revenue.append(float(row[1]))
         date.append(row[0])
-        print(revenue)
-        # row_dt = row[0]
-        # row_n = int(row[1])
-        # total_months = total_months + 1
-        # total_pl = total_pl + row_n
-     #   print(str(revenue))
+
+    # calculate the montly revenue change and populate rev_change_lst
     for i in range(int(len(revenue)-1)):
         revenue_change = (revenue[i+1]) - (revenue[i])
         rev_change_lst.append(revenue_change)
-        #print(revenue_change)
+        # use rev_change_list to calculate total months, to p&l, average monthly change, and greatest increase & decrease in profits
         sum_rev_change = sum(rev_change_lst)
         len_rev_change = len(rev_change_lst)
         avg_rev_change = sum_rev_change / len_rev_change
         max_rev_change = max(rev_change_lst)
-        max_index = rev_change_lst.index(max(rev_change_lst))
-        #print(str(max_index))
-        #max_row = max_index
-        #print(date[max_index + 1])
         min_rev_change = min(rev_change_lst)
+        # use index of max  & min rev_change to print associated month
+        max_index = rev_change_lst.index(max(rev_change_lst))
         min_index = rev_change_lst.index(min(rev_change_lst))
 
-        #print(row_dt)
-    #     total_months = row + 
-    #     total = sum(int(row[1]))
-    # print(str(total))
+# print to a new text file
+with open("Analysis/output.txt", "w") as text_file:
+    print("Financial Analysis", file=text_file)
+    print("----------------------------", file=text_file)
+    print("Total Months: " +str(len(date)), file=text_file)
+    print("Total: $" + str(round(sum(revenue))), file=text_file)
+    print("Average Change: $" + str(round(avg_rev_change)), file=text_file)
+    print("Greatest Increase in Profits: " + str(date[max_index +1]) + " $" + str(max_rev_change), file=text_file)
+    print("Greatest Decrease in Profits: " + str(date[min_index +1]) + " $" + str(min_rev_change), file=text_file)
 
-
-#the average of the changes in "Profits/Losses" over the period
-
-#the greatest increase in profits (Date and amount) over the period
-
-#the greatest decrease in losses (date and amount) over the entire period
-
-# look like this:
-# print("Total Months: " + str(total_months))
-# print("Total: $" + str(total_pl))
+# print results
+print("Financial Analysis")
+print("----------------------------")
 print("Total Months: " +str(len(date)))
 print("Total: $" + str(round(sum(revenue))))
 print("Average Change: $" + str(round(avg_rev_change)))
 print("Greatest Increase in Profits: " + str(date[max_index +1]) + " $" + str(max_rev_change))
 print("Greatest Decrease in Profits: " + str(date[min_index +1]) + " $" + str(min_rev_change))
-
-#Total Months: 86
-#Total: $38382578
-#Average Change: $-2315.12
-#Greatest Increase in Profits: Feb 2012 ($1926159)
-#Greatest Decrease in Profits: Sept-2013 (-$2196167)
-#output to a new file   
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,5 +1,6 @@
 #import csv
 import os
+#import counter function
 from collections import Counter
 csvpath = os.path.join('resources', 'election_data.csv')
 import csv
@@ -7,101 +8,40 @@ import csv
 print("Election Results")
 print("-----------------------")
 
-#set variables and lists
-# total_votes = 0
+# set list for total candidate vote counts
 can_count = []
-# candidates = {}
-# can_0_votes = 0
-# can_1_votes = 0
-# can_2_votes = 0
-# can_3_votes = 0
-# percentages = []
-
-# with open(csvpath, 'r') as text:
-#     csvreader = csv.reader(text, delimiter=',')
-#     lines=text.read()
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile)
-    #read(csvfile, delimiter=',')
+    # pass csv header
     csv_header = next(csvreader) 
-    #print(csv_header)
-#find total number of votes cast
+
+    # use a for loop to add all votes to can_count
     for row in csvreader:
         can_count.append(row[2])
-    #     row_voterid = row[0]
-    # #     row_county = row[1]
-    #     row_candidate = row[2]
-    # #     #row_candidate = text.read()
-    # #     #print(row_candidate)
-    #     if row_candidate not in candidates:
-    #         candidates.append(row_candidate)
-    #   total_votes = total_votes + 1
+    # use counter function to count all votes and create dictionary
     candidates = Counter(can_count)
+    # calculate total votes cast
     total_votes = (sum(candidates.values()))
-    print("Total Votes: " + str(total_votes))
-    print("-----------------------")
+    
+    # use max function to find the key of the candidate with the most votes
     winner = (max(candidates, key=candidates.get))
 
-    # for value in candidates.items():
-    #     print((value/total_votes)*100)
+    # print results to terminal and text file
+    with open("Analysis/output.txt", "w") as text_file:
+        print("Total Votes: " + str(total_votes))
+        print("Total Votes: " + str(total_votes), file=text_file)        
+        print("-----------------------")
+        print("-----------------------", file=text_file)
+        # use a for loop to calculate the percentage of the vote each candidate received and print
+        for key, value in candidates.items():
+            percentage = round(((value / total_votes) * 100),4 )
+            print(key + ": ", str((percentage)) + "%", value)
+            print(key + ": ", str((percentage)) + "%", value, file=text_file)
 
-    
-    for key, value in candidates.items():
-       # for i in range(len(candidates)):
-        percentage = round(((value / total_votes) * 100),4 )
-        print(key + ": ", str((percentage)) + "%", value)
-       # print(key + ": ", str(((value / total_votes) * 100)) + "%", value)
-    print("-----------------------")
-    print("Winner: " + winner)
-    print("-----------------------")
-    #print((([value for value in candidates.values()][1]) / total_votes) * 100)
-    #print(max(candidates.values()))
-    #print("Total Votes: "+ str(total_votes))
-    #print(candidates)
-    # print(row_candidate)
-    # a = range((len(can_count)))
-    # b = range((len(candidates)))
-    # for i, x in a, b: 
-    # for i in range((len(can_count))):
-    #     if can_count[i] == candidates[0]: 
-    #         can_0_votes = can_0_votes + 1
-    #     elif can_count[i] == candidates[1]:    
-    #         can_1_votes = can_1_votes + 1
-    #     elif can_count[i] == candidates[2]:    
-    #         can_2_votes = can_2_votes + 1
-    #     else:
-    #         can_3_votes = can_3_votes + 1
-
-    # percent_0 = round(can_0_votes / total_votes *100,4)
-    # percent_1 = round(can_1_votes / total_votes *100,4)
-    # percent_2 = round(can_2_votes / total_votes *100,4)
-    # percent_3 = round(can_3_votes / total_votes *100,4)
-
-    # percentages = [percent_0, percent_1, percent_2, percent_3]
-    # max_percent = percentages.index(max(percentages))
-    # winner = candidates[max_percent]
-    
-    # print(candidates[0] + ": " + str(percent_0) + "% (" + str(can_0_votes) + ")")
-    # print(candidates[1] + ": " + str(percent_1) + "% (" + str(can_1_votes) + ")")
-    # print(candidates[2] + ": " + str(percent_2) + "% (" + str(can_2_votes) + ")")
-    # print(candidates[3] + ": " + str(percent_3) + "% (" + str(can_3_votes) + ")")
-    # print("------------------------------")
-    # print("Winner: " + str(winner))
-
-    
-#compile a list of candidates who received votes
-#calculate percentages of votes each candidate won
-#total number of votes each candidate won
-#winner of election based on popular vote
-#  Election Results
-#   -------------------------
-#   Total Votes: 3521001
-#   -------------------------
-#   Khan: 63.000% (2218231)
-#   Correy: 20.000% (704200)
-#   Li: 14.000% (492940)
-#   O'Tooley: 3.000% (105630)
-#   -------------------------
-#   Winner: Khan
-#   -------------------------
+        print("-----------------------")
+        print("-----------------------", file=text_file)
+        print("Winner: " + winner)
+        print("Winner: " + winner, file=text_file)
+        print("-----------------------")
+        print("-----------------------", file=text_file)
